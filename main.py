3c46f5c9c8a11950f0561f4a5ab8b932144119de
radjah-saidi <radjahsaidi346@gmail.com>
2025-07-18 18:16:06 +0100

    affichage ligne

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("bonjour,je suis mr dev")
+def afficher_ligne(n):
+    for i in range(n):
+        print("*"*i)
+afficher_ligne(12)
 print("pour chaque modification il faut sauvegarder et meme avant de faire le git init il faut le faire aussi")
