132120d0608098bc29ee33501559595e7224ac46
radjah-saidi <radjahsaidi346@gmail.com>
2025-07-19 18:09:17 +0100

    changement par le underscrore

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ print("bonjour,je m'appelle mr dev")
 
 def afficher_ligne(n):
     for i in range(n):
-        print("*"*i)
+        print("_"*i)
 afficher_ligne(12)
+afficher_ligne(2)
 print("pour chaque modification il faut sauvegarder et meme avant de faire le git init il faut le faire aussi")
-<<<<<<< HEAD
 
 print(".. sort de ce programme;ca retourne forcement dans le desketop.")
-print("../repo_test sort de ce repertoire et va dansce repertoire-ci. ")
-=======
-print(".. sort de ce programme;ca retourne forcement dans le desketop.")
 print("../repo_test sort de ce repertoire et va dansce repertoire-ci. ") 
->>>>>>> 5343d90aebbc9f8cfd989f59808c8c68492805ac
