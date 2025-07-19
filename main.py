4c246fab51ba9c3d938cffb4f9cf92cd83d65089
radjah-saidi <radjahsaidi346@gmail.com>
2025-07-19 18:20:27 +0100

    changement par le plus et l'appel avec un un

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def afficher_ligne(n):
     for i in range(n):
-        print("*"*i)
+        print("+"*i)
 afficher_ligne(12)
+afficher_ligne(1)
 print("pour chaque modification il faut sauvegarder et meme avant de faire le git init il faut le faire aussi")
 print(".. sort de ce programme;ca retourne forcement dans le desketop.")
 print("../repo_test sort de ce repertoire et va dansce repertoire-ci. ") 
